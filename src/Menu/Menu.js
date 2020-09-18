3050f97d868af61bc9cd8a79a09d1e00cfe2bdff
cooljasonmelton <cooljasonmelton>
2020-09-18 00:04:21 -0500

    set up push to route

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -6,32 +6,36 @@ import { withRouter } from 'react-router-dom';
 // styling
 import './Menu.css';
 
-const Menu = () => {
+const Menu = props => {
     const [openMenu, setOpenMenu] = useState(false)
+
+    const pushToRoute = route => {
+        props.history.push(route)
+        setOpenMenu(false)
+    }
 
     const setClassNames = num => {
         const classArr = ["m-item"];
         if (openMenu) classArr.push(`open-${num}`)
-
         return classArr.join(' ')
     }
 
     return (
         <div className="Menu">
-            <div className={setClassNames(1) + " m-logo"}
+            <div className={"m-item m-logo"}
                 onClick={() => setOpenMenu(!openMenu)}>
-                Home
+                Menu
             </div>
-            <div className={setClassNames(2)}
-                onClick={null}>
+            <div className={setClassNames(1)}
+                onClick={() => pushToRoute("/dashboard")}>
                 Dashboard
             </div>
-            <div className={setClassNames(3)}
-                onClick={null}>
+            <div className={setClassNames(2)}
+                onClick={() => pushToRoute("/settings")}>
                 Settings
             </div>
-            <div className={setClassNames(4)}
-                onClick={null}>
+            <div className={setClassNames(3)}
+                onClick={() => pushToRoute("/")}>
                 Sign out
             </div>
         </div>
